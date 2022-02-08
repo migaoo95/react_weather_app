70bb76c35532ff88612c46eb0110926d8c629f59
migaoo95 <45981784+migaoo95@users.noreply.github.com>
2022-02-08 19:06:10 +0000

    Build server test

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import { WeatherContextProvider } from "./components/context/WeatherContext";
 function App() {
   return (
     <WeatherContextProvider>
-      <div className="App container flex h-screen m-auto">
-        <div className="container md:m-auto  rounded-xl sm:h-[100%]    md:w-[60%] md:h-[65%] md:min-w-[795px] md:max-h-[600px] grid sm:grid-cols-1 md:grid-cols-2">
-          <MainWeather />
-          <WeatherDetails />
+      <div className="main-container-app glass">
+        <div className="App container flex h-screen m-auto">
+          <div className="container md:m-auto  rounded-xl sm:h-[100%]    md:w-[60%] md:h-[65%] md:min-w-[795px] md:max-h-[600px] grid sm:grid-cols-1 md:grid-cols-2">
+            <MainWeather />
+            <WeatherDetails />
+          </div>
         </div>
       </div>
     </WeatherContextProvider>
